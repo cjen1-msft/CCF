9fc8332fed383ebfa20e0c1c0bdab14f921b8c88
cjen1-msft <chrisjensen@microsoft.com>
2025-05-15 18:31:54 +0100

    Ensure nodes pass through target_rpc_addresses

--- src/host/main.cpp
+++ src/host/main.cpp
@@ -818,6 +818,9 @@ int main(int argc, char** argv) // NOLINT(bugprone-exception-escape)
         startup_config.recover.previous_sealed_ledger_secret_location =
           config.command.recover.previous_sealed_ledger_secret_location;
       }
+
+      startup_config.recover.auto_dr_target_rpc_addresses =
+        config.command.recover.auto_dr_target_rpc_addresses;
     }
     else
     {
