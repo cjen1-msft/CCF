345af9c28a93aecd26564fd7216a5c9838e25059
Max <maxtropets@microsoft.com>
2026-04-28 09:39:29 +0100

    Update Gersemi and make it fail on warnings (#7842)

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -85,8 +85,7 @@ endfunction()
 # Helper for building end-to-end function tests using the python infrastructure
 function(add_e2e_test)
   cmake_parse_arguments(
-    PARSE_ARGV
-    0
+    PARSE_ARGV 0
     PARSED_ARGS
     ""
     "NAME;PYTHON_SCRIPT;LABEL;CURL_CLIENT"
@@ -190,8 +189,7 @@ endfunction()
 # Helper for building end-to-end perf tests using the python infrastucture
 function(add_piccolo_test)
   cmake_parse_arguments(
-    PARSE_ARGV
-    0
+    PARSE_ARGV 0
     PARSED_ARGS
     ""
     "NAME;PYTHON_SCRIPT;CONSTITUTION;CLIENT_BIN;PERF_LABEL"
@@ -233,8 +231,7 @@ endfunction()
 # Picobench wrapper
 function(add_picobench name)
   cmake_parse_arguments(
-    PARSE_ARGV
-    1
+    PARSE_ARGV 1
     PARSED_ARGS
     ""
     ""
